51b397dbf27b43f47deab22a6f864e7fb6ec866d
Harsh Kalam <rajharsh7866@gmail.com>
2024-07-26 18:54:44 +0530

    unhide

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -16,7 +16,7 @@ const Banner = () => {
                   mollitia aliquam reprehenderit? Magnam, odit est voluptates perferendis
                   aperiam nisi unde!</p>
           </div>
-          <div className='right-img hidden'>
+          <div className='right-img'>
             <img src= {pizza} alt="" className='h-auto w-[450px] ml-36 animate-spin-slow'/>
           </div>
       </div>
